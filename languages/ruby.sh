6cb99bd051e3141abda0d3c7042dcab471d3b6e1
Andrew Klotz <agc.klotz@gmail.com>
2017-01-18 21:09:16 -0500

    osx specfic setup

--- languages/ruby.sh
+++ languages/ruby.sh
@@ -2,28 +2,19 @@
 
 set -ex
 
-echo '# installing ruby deps'
-# https://github.com/rbenv/ruby-build/wiki#suggested-build-environment
-sudo apt-get install autoconf \
-                bison \
-                build-essential \
-                libssl-dev \
-                libyaml-dev \
-                libreadline6-dev \
-                zlib1g-dev \
-                libncurses5-dev \
-                libffi-dev \
-                libgdbm3 \
-                libgdbm-dev
+# https://github.com/sstephenson/ruby-build/wiki#suggested-build-environment
+brew update
+brew install openssl libyaml libffi
 
 echo '# installing rvm'
 gpg --keyserver hkp://keys.gnupg.net --recv-keys 409B6B1796C275462A1703113804BB82D39DC0E3
 \curl -sSL https://get.rvm.io | bash -s stable --ruby
-# source bash_profile?
-# echo "source $HOME/.rvm/scripts/rvm" >> ~/.bashrc
 
 echo '# installint rubies'
 rvm install 2.2.6
 rvm install 2.3.3
 rvm install 2.4.0
 rvm --default use 2.4.0
+
+gem install bundler
+
--- setup.sh
+++ setup.sh
@@ -2,16 +2,9 @@
 
 set -ex
 
-sudo apt-get update
-sudo apt-get install git \
-                     zsh \
-                     git-core \
-                     tmux \
-                     vim
-
 # need ruby for scripting
 sh languages/ruby.sh
 
 # editor stuff
-# sh -c "$(curl -fsSL https://raw.githubusercontent.com/robbyrussell/oh-my-zsh/master/tools/install.sh)"
 sh -c "`curl -fsSL https://raw.githubusercontent.com/skwp/dotfiles/master/install.sh`"
+
