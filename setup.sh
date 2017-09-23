8b1d5d1d218d25980eff2b8f5460df2228de820c
Andrew Klotz <agc.klotz@gmail.com>
2017-09-23 16:37:10 -0400

    puma-dev

--- setup.sh
+++ setup.sh
@@ -6,9 +6,13 @@ set -ex
 brew update
 brew install shellcheck
 
+# just ~/.puma-dev/ instead of ~/pow/
+brew install puma/puma/puma-dev
+puma-dev -install
+sudo puma-dev -setup
+
 # need ruby for scripting
 sh languages/ruby.sh
 
 # editor stuff
 sh -c "`curl -fsSL https://raw.githubusercontent.com/skwp/dotfiles/master/install.sh`"
-
