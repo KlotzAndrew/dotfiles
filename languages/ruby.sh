e487270b77de35f65a366268d90bba5f629aa679
Andrew Klotz <agc.klotz@gmail.com>
2017-09-23 11:37:05 -0400

    skip older rubies

--- languages/ruby.sh
+++ languages/ruby.sh
@@ -10,10 +10,8 @@ echo '# installing rvm'
 gpg --keyserver hkp://keys.gnupg.net --recv-keys 409B6B1796C275462A1703113804BB82D39DC0E3
 \curl -sSL https://get.rvm.io | bash -s stable --ruby
 
-echo '# installint rubies'
-rvm install 2.2.6
+echo '# installing rubies'
 rvm install 2.3.3
-rvm install 2.4.0
 rvm install 2.4.1
 rvm --default use 2.4.1
 
