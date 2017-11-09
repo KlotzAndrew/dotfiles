cd4597c27518db1d57e557bbe1853ad8363e278a
Andrew Klotz <agc.klotz@gmail.com>
2017-11-09 00:35:53 -0500

    some useful brew

--- setup.sh
+++ setup.sh
@@ -5,11 +5,19 @@ set -ex
 # osx setup...
 brew update
 brew install shellcheck
+brew install postgresql
+brew install neo4j
+brew install memcached
+brew install redis
+brew install awscli
+brew install youtube-dl
 
 # just ~/.puma-dev/ instead of ~/pow/
 brew install puma/puma/puma-dev
 puma-dev -install
 sudo puma-dev -setup
+
+brew cask install slack
 
 # need ruby for scripting
 sh languages/ruby.sh
